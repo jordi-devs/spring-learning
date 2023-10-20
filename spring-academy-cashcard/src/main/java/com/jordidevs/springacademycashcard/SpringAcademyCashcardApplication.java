7bc2396b51bd83c1003ea58c8df676c0179a8d51
jordi-devs <30882236+jordi-devs@users.noreply.github.com>
2023-10-20 21:08:06 +0300

    spring academy cashcard project structure

---
+++ spring-academy-cashcard/src/main/java/com/jordidevs/springacademycashcard/SpringAcademyCashcardApplication.java
@@ -0,0 +1,13 @@
+package com.jordidevs.springacademycashcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAcademyCashcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAcademyCashcardApplication.class, args);
+	}
+
+}
---
+++ spring-academy-cashcard/src/test/java/com/jordidevs/springacademycashcard/SpringAcademyCashcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordidevs.springacademycashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAcademyCashcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
