8b9c3e2a02224612e2a81af34cd2ac5ee616e504
jordi-devs <30882236+jordi-devs@users.noreply.github.com>
2023-10-26 18:07:30 +0300

    add findAll paging and sorting

--- spring-academy-cashcard/src/main/java/com/jordidevs/springacademycashcard/CashCardController.java
+++ spring-academy-cashcard/src/main/java/com/jordidevs/springacademycashcard/CashCardController.java
@@ -1,10 +1,15 @@
 package com.jordidevs.springacademycashcard;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -21,6 +26,17 @@ public class CashCardController {
     public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
         Optional<CashCard> cashCard = cashCardRepository.findById(requestedId);
         return cashCard.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
+        Page<CashCard> page = cashCardRepository.findAll(
+                PageRequest.of(
+                        pageable.getPageNumber(),
+                        pageable.getPageSize(),
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+                ));
+        return ResponseEntity.ok(page.getContent());
     }
 
     @PostMapping
