3ffbde2c94fee49c7fcbd25e1a5741c7b43dd8c9
jordi-devs <30882236+jordi-devs@users.noreply.github.com>
2023-10-26 19:05:03 +0300

    refactor lambda

--- spring-academy-cashcard/src/main/java/com/jordidevs/springacademycashcard/SecurityConfig.java
+++ spring-academy-cashcard/src/main/java/com/jordidevs/springacademycashcard/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -20,7 +21,7 @@ public class SecurityConfig {
         http.authorizeHttpRequests(request -> request
                         .requestMatchers("/cashcards/**")
                         .hasRole("CARD-OWNER"))
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
